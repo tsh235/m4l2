0cfd993de9ec3e81dbd4943fc529829a2b01275e
tsh235 <tanyashustrova@yandex.ru>
2024-02-18 23:57:55 +0300

    final

--- cart.js
+++ cart.js
@@ -1,55 +1,80 @@
 'use strict';
 
-// Cart
-
 const Cart = function() {
   this.goods = [];
-  console.log('this.goods: ', this.goods);
+  this.count = 0;
+  this.totalPrice = 0;
+
+  this.addGoods = function(product, count = 1) {
+    this.goods.push(product);
+    this.increaseCount(count);
+  };
+};
+
+const cart = new Cart();
+
+Cart.prototype.increaseCount = function(count) {
+  return this.count += count;
+};
+
+Cart.prototype.calculateGoodsPrice = function() {
+  let totalPrice = 0;
+  this.goods.forEach(item => {
+    totalPrice += item.price;
+  });
+
+  return this.totalPrice = totalPrice;
+};
+
+Cart.prototype.print = function() {
+  console.log(JSON.stringify(cart.goods));
+  console.log(`Общая стоимость корзины составляет ${cart.totalPrice}`);
+};
+
+Cart.prototype.clear = function() {
+  this.goods = [];
   this.count = 0;
   this.totalPrice = 0;
 };
 
-Cart.addGoods = function(good) {
-  console.log('this.goods: ', this.goods);
-  this.goods.push(good);
-};
-
-Cart.increaseCount = function(count) {
-  return this.count += count;
-};
-
-// Goods
-
 const Goods = function(name, price, discount) {
-  Cart.call(this, name, price, discount);
   this.name = name;
   this.price = price;
   this.discount = discount;
-  return this;
 };
 
 const FoodGoods = function(name, price, discount, calories) {
-  Goods.call(this, name, price, discount);
-  console.log('this: ', this);
-  this.calories = calories;
+  return {
+    name,
+    price,
+    discount,
+    calories,
+  };
 };
 
 const ClothingGoods = function(name, price, discount, material) {
-  Goods.call(this, name, price, discount, material);
-  this.material = material;
+  return {
+    name,
+    price,
+    discount,
+    material,
+  };
 };
 
 const TechnicsGoods = function(name, price, discount, type) {
-  Goods.call(this, name, price, discount);
-  this.type = type;
+  return {
+    name,
+    price,
+    discount,
+    type,
+  };
 };
 
-const p1 = new FoodGoods('Молоко', 50, 10, 80);
-console.log('p1: ', p1);
-Cart.addGoods(p1);
-const p2 = new ClothingGoods('Кофта', 20, 0, 'Шерсть');
-Cart.addGoods(p2);
-const p3 = new TechnicsGoods('Телефон', 20, 0, 'Смартфон');
-Cart.addGoods(p3);
-
-
+cart.addGoods(new FoodGoods('Молоко', 50, 10, 80));
+cart.addGoods(new ClothingGoods('Кофта', 1500, 0, 'Шерсть'));
+cart.addGoods(new TechnicsGoods('Телефон', 20000, 3, 'Смартфон'));
+cart.addGoods(new TechnicsGoods('Телефизор', 60000, 0, 'Быт техника'));
+cart.calculateGoodsPrice();
+cart.print();
+cart.clear();
+console.log(cart);
